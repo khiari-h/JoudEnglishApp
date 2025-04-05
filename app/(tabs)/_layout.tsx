c391cdb4207c7490f0434639a79e376991ead200
Hamdane Khiari <hamdane.khiari@gmail.com>
2025-04-05 09:28:57 +0200

    feat: Mise à jour dashdoard.tsx dans app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,27 +4,14 @@ import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: '#000', // Met ton propre style ici
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
+        tabBarButton: HapticTab, // Si tu as un composant personnalisé, garde-le
       }}>
       <Tabs.Screen
         name="index"
