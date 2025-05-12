1c8c5cce7f1f6cd073e9b17420a01386fc261709
Hamdane Khiari <hamdane.khiari@gmail.com>
2025-05-12 18:41:16 +0200

    feat:Add vocabulary A2

--- src/data/vocabulary/A2/index.js
+++ src/data/vocabulary/A2/index.js
@@ -1,73 +1,35 @@
-import a2_voc_abstract_concepts from "./categories/a2_voc_abstract_concepts.js";
-import a2_voc_accessories_styles from "./categories/a2_voc_accessories_styles.js";
-import a2_voc_actions_movements from "./categories/a2_voc_actions_movements.js";
-import a2_voc_advanced_daily_activities from "./categories/a2_voc_advanced_daily_activities.js";
-import a2_voc_advanced_greetings from "./categories/a2_voc_advanced_greetings.js";
-import a2_voc_advanced_weather from "./categories/a2_voc_advanced_weather.js";
-import a2_voc_adverbs from "./categories/a2_voc_adverbs.js";
-import a2_voc_banking_money from "./categories/a2_voc_banking_money.js";
-import a2_voc_comparative_superlative from "./categories/a2_voc_comparative_superlative.js";
-import a2_voc_conversational_expressions from "./categories/a2_voc_conversational_expressions.js";
-import a2_voc_descriptions from "./categories/a2_voc_descriptions.js";
-import a2_voc_digital_communication from "./categories/a2_voc_digital_communication.js";
-import a2_voc_education from "./categories/a2_voc_education.js";
-import a2_voc_emergency_safety from "./categories/a2_voc_emergency_safety.js";
-import a2_voc_entertainment_leisure from "./categories/a2_voc_entertainment_leisure.js";
-import a2_voc_environment_nature from "./categories/a2_voc_environment_nature.js";
-import a2_voc_frequency_duration from "./categories/a2_voc_frequency_duration.js";
-import a2_voc_geography from "./categories/a2_voc_geography.js";
-import a2_voc_health_illness from "./categories/a2_voc_health_illness.js";
-import a2_voc_holidays_celebrations from "./categories/a2_voc_holidays_celebrations.js";
-import a2_voc_home_items from "./categories/a2_voc_home_items.js";
-import a2_voc_life_events from "./categories/a2_voc_life_events.js";
-import a2_voc_more_food_drinks from "./categories/a2_voc_more_food_drinks.js";
-import a2_voc_negative_expressions from "./categories/a2_voc_negative_expressions.js";
-import a2_voc_phrasal_verbs from "./categories/a2_voc_phrasal_verbs.js";
-import a2_voc_places_town from "./categories/a2_voc_places_town.js";
-import a2_voc_quantities_measurements from "./categories/a2_voc_quantities_measurements.js";
-import a2_voc_relationships from "./categories/a2_voc_relationships.js";
-import a2_voc_spatial_relations from "./categories/a2_voc_spatial_relations.js";
-import a2_voc_sports_hobbies from "./categories/a2_voc_sports_hobbies.js";
-import a2_voc_technology_media from "./categories/a2_voc_technology_media.js";
-import a2_voc_time_expressions from "./categories/a2_voc_time_expressions.js";
-import a2_voc_travel_tourism from "./categories/a2_voc_travel_tourism.js";
-import a2_voc_work_environment from "./categories/a2_voc_work_environment.js";
+import { vocab as identite_elargie } from "./categories/01_identite_elargie.js";
+import { vocab as famille_relations } from "./categories/02_famille_relations.js";
+import { vocab as logement_environnement } from "./categories/03_logement_environnement.js";
+import { vocab as urbain_rural } from "./categories/04_urbain_rural.js";
+import { vocab as travail_professionnel } from "./categories/05_travail_professionnel.js";
+import { vocab as alimentation_elargie } from "./categories/06_alimentation_elargie.js";
+import { vocab as sante_corps } from "./categories/07_sante_corps.js";
+import { vocab as achats_services } from "./categories/08_achats_services.js";
+import { vocab as transports_voyages } from "./categories/09_transports_voyages.js";
+import { vocab as education_apprentissage } from "./categories/10_education_apprentissage.js";
+import { vocab as loisirs_culture } from "./categories/11_loisirs_culture.js";
+import { vocab as temps_meteo } from "./categories/12_temps_meteo.js";
+import { vocab as communications } from "./categories/13_communications.js";
+import { vocab as nature_environnement } from "./categories/14_nature_environnement.js";
+import { vocab as actualites_evenements } from "./categories/15_actualites_evenements.js";
 
 export default {
   exercises: [
-    a2_voc_abstract_concepts,
-    a2_voc_accessories_styles,
-    a2_voc_actions_movements,
-    a2_voc_advanced_daily_activities,
-    a2_voc_advanced_greetings,
-    a2_voc_advanced_weather,
-    a2_voc_adverbs,
-    a2_voc_banking_money,
-    a2_voc_comparative_superlative,
-    a2_voc_conversational_expressions,
-    a2_voc_descriptions,
-    a2_voc_digital_communication,
-    a2_voc_education,
-    a2_voc_emergency_safety,
-    a2_voc_entertainment_leisure,
-    a2_voc_environment_nature,
-    a2_voc_frequency_duration,
-    a2_voc_geography,
-    a2_voc_health_illness,
-    a2_voc_holidays_celebrations,
-    a2_voc_home_items,
-    a2_voc_life_events,
-    a2_voc_more_food_drinks,
-    a2_voc_negative_expressions,
-    a2_voc_phrasal_verbs,
-    a2_voc_places_town,
-    a2_voc_quantities_measurements,
-    a2_voc_relationships,
-    a2_voc_spatial_relations,
-    a2_voc_sports_hobbies,
-    a2_voc_technology_media,
-    a2_voc_time_expressions,
-    a2_voc_travel_tourism,
-    a2_voc_work_environment
+    identite_elargie,
+    famille_relations,
+    logement_environnement,
+    urbain_rural,
+    travail_professionnel,
+    alimentation_elargie,
+    sante_corps,
+    achats_services,
+    transports_voyages,
+    education_apprentissage,
+    loisirs_culture,
+    temps_meteo,
+    communications,
+    nature_environnement,
+    actualites_evenements
   ],
 };
