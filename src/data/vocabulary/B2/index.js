defe24b5c4f6c1add2020e5a44e9c27dca95bd92
Hamdane Khiari <hamdane.khiari@gmail.com>
2025-05-15 06:25:37 +0200

    feat:Ajout index niveau B2

---
+++ src/data/vocabulary/B2/index.js
@@ -0,0 +1,35 @@
+import { vocab as identite_complexe_et_psychologie } from "./categories/01_identite_complexe_et_psychologie.js";
+import { vocab as relations_et_dynamiques_sociales } from "./categories/02_relations_et_dynamiques_sociales.js";
+import { vocab as economie_et_monde_des_affaires } from "./categories/03_economie_et_monde_des_affaires.js";
+import { vocab as urbanisme_et_amenagement_du_territoire } from "./categories/04_urbanisme_et_amenagement_du_territoire.js";
+import { vocab as sante_publique_et_systemes_de_sante } from "./categories/05_sante_publique_et_systemes_de_sante.js";
+import { vocab as technologies_de_l_information_et_communication } from "./categories/06_technologies_de_l_information_et_communication.js";
+import { vocab as questions_environnementales_avancees } from "./categories/07_questions_environnementales_avancees.js";
+import { vocab as politique_et_gouvernance } from "./categories/08_politique_et_gouvernance.js";
+import { vocab as justice_et_droit } from "./categories/09_justice_et_droit.js";
+import { vocab as education_superieure_et_recherche } from "./categories/10_education_superieure_et_recherche.js";
+import { vocab as arts_et_critique_culturelle } from "./categories/11_arts_et_critique_culturelle.js";
+import { vocab as medias_avances_et_communication_de_masse } from "./categories/12_medias_avances_et_communication_de_masse.js";
+import { vocab as debats_societaux_contemporains } from "./categories/13_debats_societaux_contemporains.js";
+import { vocab as economie_globale_et_finances } from "./categories/14_economie_globale_et_finances.js";
+import { vocab as sciences_et_applications_technologiques } from "./categories/15_sciences_et_applications_technologiques.js";
+
+export default {
+  exercises: [
+    identite_complexe_et_psychologie,
+    relations_et_dynamiques_sociales,
+    economie_et_monde_des_affaires,
+    urbanisme_et_amenagement_du_territoire,
+    sante_publique_et_systemes_de_sante,
+    technologies_de_l_information_et_communication,
+    questions_environnementales_avancees,
+    politique_et_gouvernance,
+    justice_et_droit,
+    education_superieure_et_recherche,
+    arts_et_critique_culturelle,
+    medias_avances_et_communication_de_masse,
+    debats_societaux_contemporains,
+    economie_globale_et_finances,
+    sciences_et_applications_technologiques
+  ],
+};
