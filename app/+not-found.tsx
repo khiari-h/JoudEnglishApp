f9f2dd707481c0c484a83a10bd00032a28ed273d
Hamdane Khiari <hamdane.khiari@gmail.com>
2025-07-14 14:37:43 +0200

    feat:Correction tabs pour not found

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFoundScreen() {
       <Stack.Screen options={{ title: "Oops!" }} />
       <View style={styles.container}>
         <Text style={styles.title}>Cette page n'existe pas.</Text>
-        <Link href="/(tabs)/dashboard" style={styles.link}>
+        <Link href="/tabs/dashboard" style={styles.link}>
           <Text style={styles.linkText}>Retour au Dashboard</Text>
         </Link>
       </View>
