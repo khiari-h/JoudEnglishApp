28134f7f16d2e56f7d3d546c1147eed9d9b853d5
Hamdane Khiari <hamdane.khiari@gmail.com>
2025-07-18 20:18:21 +0200

    feat:styles

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,20 +1,6 @@
 import { Link, Stack } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: "Oops!" }} />
-      <View style={styles.container}>
-        <Text style={styles.title}>Cette page n&apos;existe pas.</Text>
-        <Link href="/tabs/dashboard" style={styles.link}>
-          <Text style={styles.linkText}>Retour au Dashboard</Text>
-        </Link>
-      </View>
-    </>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -34,3 +20,17 @@ const styles = StyleSheet.create({
     color: "blue",
   },
 });
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Oops!" }} />
+      <View style={styles.container}>
+        <Text style={styles.title}>Cette page n&apos;existe pas.</Text>
+        <Link href="/tabs/dashboard" style={styles.link}>
+          <Text style={styles.linkText}>Retour au Dashboard</Text>
+        </Link>
+      </View>
+    </>
+  );
+}
